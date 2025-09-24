519566d438ef58262f365024b6c254c30820aac6
Let-Varchar <36794713+Let-Varchar@users.noreply.github.com>
2025-09-24 20:30:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ self.addEventListener("install", e => {
     })
   );
 });
-
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
